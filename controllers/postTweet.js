3839049cfab9ba135d39ca3a752e604ec14ebe9e
Kaami Dev <kaami@kaamidev.com>
2020-07-21 02:20:47 -0400

    some more commenting

--- controllers/postTweet.js
+++ controllers/postTweet.js
@@ -1,5 +1,6 @@
 const Twit = require('twit');
 
+// twitter api key information
 let T = new Twit({
 	consumer_key: process.env.CONSUMER_KEY,
 	consumer_secret: process.env.CONSUMER_SECRET,
@@ -10,15 +11,18 @@ let T = new Twit({
 });
 
 const postTweet = (word, image) => {
+	// starts by uploading media to twitter
 	T.post('media/upload', { media_data: image }, function(err, data, response) {
 		let mediaIdStr = data.media_id_string;
 		let meta_params = { media_id: mediaIdStr };
 
+		// creates media metadeta from params specified after upload
 		T.post('media/metadata/create', meta_params, function(err, data, response) {
 			if (!err) {
-				// now we can reference the media and post a tweet (media will attach to the tweet)
+				// assuming no errors, it then creates a tweet status with the image uploaded attached
 				let params = { status: 'Today\'s #WordOfTheDay is "' + word + '"!', media_ids: [ mediaIdStr ] };
 
+				// the tweet is then posted and logged accordingly to the console
 				T.post('statuses/update', params, function(err, data, response) {
 					console.log('Tweeted Successfully! The word was ' + word);
 				});
