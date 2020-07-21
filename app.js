8f60f1f93f7b4b72243090c829f9bf45ab15ef3a
Kaami Dev <kaami@kaamidev.com>
2020-07-21 02:29:49 -0400

    added cron job functionality

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 require('dotenv').config();
+const CronJob = require('cron').CronJob;
+
 const getWord = require('./controllers/getWord.js');
 const getImage = require('./controllers/getImage.js');
 const convertImage = require('./controllers/convertImage.js');
 const postTweet = require('./controllers/postTweet.js');
 
+// setup cron job to tweet a new word everyday at 12PM (server time)
+let job = new CronJob('0 12 * * *', function() {
+	// call make tweet function
+	makeTweet();
+});
+
+// start cron job
+job.start();
+console.log('Cron job started, running every day at 12PM...');
+
+// define make tweet function
 const makeTweet = async () => {
 	// gets random word and definiton info
 	let word = await getWord();
@@ -17,4 +30,3 @@ const makeTweet = async () => {
 	// tweets everything out
 	postTweet(word.word, image);
 };
-makeTweet();
